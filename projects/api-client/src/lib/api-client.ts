4c8a895a5e80b06867f15c2530121874e01630f5
madel <mohammed.adel.abo.foutouh@gmail.com>
2026-08-09 17:12:40 +0300

    first commit

---
+++ projects/api-client/src/lib/api-client.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-api-client',
+  imports: [],
+  template: ` <p>api-client works!</p> `,
+  styles: ``,
+})
+export class ApiClient {}
---
+++ projects/api-client/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of api-client
+ */
+
+export * from './lib/api-client';
